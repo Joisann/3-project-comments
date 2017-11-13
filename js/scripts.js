058d2fb1c8a4f30b97e2e10b6f394b390aa3c35f
Joisann <joisann777@gmail.com>
2017-11-13 20:45:46 +0100

    adding files

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// scripts.js
+
+var name = prompt('Enter your name');
+//alert('Hello, ' + name);
+/*Komentarz na kilka linii 
+Komentarz na kilka linii 
+Komentarz na kilka linii 
+Komentarz na kilka linii */
+
+console.log('Hello, ' + name);
+
+// Komentarz na jedną linie 
